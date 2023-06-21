a0fb8a4cf0385782b6e5fc9a97a47a2021b5015d
mihneabuz <mihneabuzatu88@gmail.com>
2023-06-21 12:38:46 +0300

    server static files from server

--- sock/server.js
+++ sock/server.js
@@ -1,14 +1,25 @@
 
+import express from "express";
+import http from "http";
 import { WebSocketServer } from "ws";
 import { parse } from "url";
 
 const seed = Math.floor(Math.random() * 100000);
 console.log(seed);
 
-const wss = new WebSocketServer({ port: 3000 });
+const app = express();
+app.use(express.static('../client'));
+
+const wss = new WebSocketServer({ noServer: true });
+
+const server = http.createServer(app);
+server.on('upgrade', (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, function done(ws) {
+    wss.emit('connection', ws, request);
+  });
+})
 
 const players = new Map();
-const bullets = []
 
 wss.on('connection', (ws, req) => {
   const name = parse(req.url ?? "", true).query.name;
@@ -50,3 +61,5 @@ setInterval(() => {
 setInterval(() => {
   console.log(players.size);
 }, 3000);
+
+server.listen(3000);
